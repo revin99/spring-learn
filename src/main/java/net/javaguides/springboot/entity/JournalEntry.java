f6193ea3bdc2c97efd8a96deedb00bee1a70203c
revin99 <revingohil@gmail.com>
2025-07-18 08:48:15 +0530

    using lombok dependency

--- src/main/java/net/javaguides/springboot/entity/JournalEntry.java
+++ src/main/java/net/javaguides/springboot/entity/JournalEntry.java
@@ -1,5 +1,9 @@
 package net.javaguides.springboot.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.cglib.core.Local;
 import org.springframework.data.annotation.Id;
@@ -8,6 +12,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 import java.util.Date;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
 @Document(collection = "journal_entries")
 public class JournalEntry {
 
@@ -15,38 +25,39 @@ public class JournalEntry {
     private ObjectId id;
     private String title;
     private String content;
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
     private LocalDateTime date;
 
-    public ObjectId getId() {
-        return id;
-    }
+//    public LocalDateTime getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(LocalDateTime date) {
+//        this.date = date;
+//    }
+//
 
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
+//
+//    public ObjectId getId() {
+//        return id;
+//    }
+//
+//    public void setId(ObjectId id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
 }
